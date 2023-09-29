bcc0b1043f31333f550cdaccc060fd97b5de0aa1
David Jaiyeola <davidjaiye247@gmail.com>
2023-09-29 04:28:35 -0400

    Updated calculator_refactor.js file.

--- calculator_refactor.js
+++ calculator_refactor.js
@@ -3,40 +3,50 @@ const readline = require('readline-sync');
 function prompt (msg) {
   console.log(`==> ${msg}`);
 }
+
+function invalidNumber (number) {
+  return number.trimStart() === '' || Number.isNaN(number); 
+}
 prompt('Welcome to calculator');
 
 prompt('What is the first number?');
-let number1 = Number(readline.question());
+let number1 = readline.question();
 
 while (invalidNumber(number1)) {
-  
+  prompt('Enter valid first number? ');
+  number1 = readline.question();
 }
 
 prompt('What is the second number');
-let number2 = Number(readline.question());
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt('Enter valid second number? ');
+  number2 = readline.question();
+}
 
 prompt('What operation would you like to perform ? \n 1) Add 2) Subtract 3) Mulitply 4) Divide');
 let operation = readline.question();
 
-let output;
+while (!['1','2','3','4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = readline.question();
+}
 
+let output;
 switch (operation) {
   case '1':
-    output = number1 + number2;
+    output = Number(number1) + Number(number2);
     break;
   case '2':
-    output = number1 - number2;
+    output = Number(number1) - Number(number2);
     break;
   case '3':
-    output = number1 * number2;
+    output = Number(number1) * Number(number2);
     break;
   case '4':
-    output = number1 * number2;
+    output = Number(number1) * Number(number2);
     break;
-  default:
-    'Invalid operation';
-
-
 }
 
 prompt(`The result is ${output}.`);
