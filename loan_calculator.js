6c18abc6b1585db98791889fc065e3f8821eba13
David Jaiyeola <davidjaiye247@gmail.com>
2023-10-29 12:02:40 -0400

    Updated loan calculator.

--- loan_calculator.js
+++ loan_calculator.js
@@ -1,46 +1,50 @@
 const readline = require('readline-sync');
 
 
-let invalidNumber = function(input) {
-return input.trim() === '' || 
-       Number.isNaN(Number(input)) ||
-       Number(input) < 0 ;
+let invalidNumber = function (input) {
+    return input.trim() === '' ||
+        Number.isNaN(Number(input)) ||
+        Number(input) < 0;
 }
 
 while (true) {
-let loanAmount = readline.question('Enter loan amount: ');
+    let loanAmount = readline.question('Enter loan amount: ');
 
-while(invalidNumber(loanAmount)){
-    console.log('Invalid loan amount was Entered.')
-    loanAmount = readline.question('Enter valid loan amount: ');
-}
+    while (invalidNumber(loanAmount)) {
+        console.log('Invalid loan amount was Entered.')
+        loanAmount = readline.question('Enter valid loan amount: ');
+    }
 
-let annualPercentageRate = readline.question('Enter annual percentage rate(apr): ');
+    let annualPercentageRate = readline.question('Enter annual percentage rate(apr): ');
 
-while (invalidNumber(annualPercentageRate)) {
-console.log('Invalid input for annual percentage rate!');
-annualPercentageRate = readling.question('Enter valid annual percentage amount:');
-}
+    while (invalidNumber(annualPercentageRate)) {
+        console.log('Invalid input for annual percentage rate!');
+        annualPercentageRate = readling.question('Enter valid annual percentage amount:');
+    }
 
-let loanDuration = readline.question('Enter loan duration (In years): ');
+    let loanDuration = readline.question('Enter loan duration (In years): ');
 
-while (invalidNumber(loanDuration)) {
-console.log('Invalid input for loan duration!');
-loanDuration = readline.question('Enter valid loan duration in years.')
-}
+    while (invalidNumber(loanDuration)) {
+        console.log('Invalid input for loan duration!');
+        loanDuration = readline.question('Enter valid loan duration in years.')
+    }
 
-let monthlyInterest = annualPercentageRate / 12;
+    let monthlyInterest = annualPercentageRate / 12;
 
-let loanDurationMonths = loanDuration / 12;
+    let loanDurationMonths = loanDuration / 12;
 
-let monthlyPayment = loanAmount * (monthlyInterest / (1 - Math.pow((1 + monthlyInterest), (-loanDurationMonths))));
+    let monthlyPayment = loanAmount * (monthlyInterest / (1 - Math.pow((1 + monthlyInterest), (-loanDurationMonths))));
 
-console.log(`Monthly payment is ${monthlyPayment.toFixed(2)}`);
+    console.log(`Monthly payment is ${monthlyPayment.toFixed(2)}`);
 
-let calculatorExit = readline.question('Calculate another payment? y - for Yes or n - for No: ');
+    let calculatorExit = readline.question('N to exit or any key to continue: ');
 
-if (calculatorExit ==='n') {
-    break;
-}
+    console.log(calculatorExit);
+    console.log(typeof calculatorExit);
+
+    if (calculatorExit.toLowerCase() === 'n') {
+        break;
+
+    }
 
 }
